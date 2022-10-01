6dd2d2c291e29c3d31646ab5104bd1886163c0b7
bquach1 <quachbruce@gmail.com>
2022-10-01 16:23:49 -0500

    Add return functionality and stylings

--- pawsyourday/src/screens/alarm.js
+++ pawsyourday/src/screens/alarm.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {ArrowLeft} from 'react-feather';
+import { NavigationType, useNavigate } from "react-router-dom";
   
 const Alarm = () => {
+  let navigate = useNavigate();
   return (
     <div class="wrapper">
+        <ArrowLeft class="navigation-arrow" onClick={() => {{(navigate('/landing'))}}}/>
       <h1>Alarm clock page.</h1>
     </div>
   );
--- pawsyourday/src/screens/exercise.js
+++ pawsyourday/src/screens/exercise.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Slider from '../components/sliders/slider';
 import '../components/css/exercise.css';
 import Squats from '../components/images/squats.png';
+import { ArrowLeft } from 'react-feather';
+import { NavigationType, useNavigate } from "react-router-dom";
   
 const Exercise = () => {
+  let navigate = useNavigate();
   return (
     <div class="exercise">
+      <ArrowLeft onClick={() => {{(navigate('/landing'))}}}/>
       <h1 class="title">Geaux Exercise!</h1>
       <div>
         <h2>Please rate the intensity of your day.</h2>
         <Slider />
       </div>
-      <img class="images" src={Squats} alt="squatting man" />
     </div>
   );
 };
