0c7199eddf1f452dc203f13a55b688f654a59bc2
bquach1 <quachbruce@gmail.com>
2022-10-01 11:06:21 -0500

    Basic landing page layout

--- pawsyourday/src/App.js
+++ pawsyourday/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './components/css/landing.css';
 import AppRouter from './AppRouter';
 import AlarmPage from './screens/alarm';
 import LandingPage from './screens/landing';
--- pawsyourday/src/screens/landing.js
+++ pawsyourday/src/screens/landing.js
@@ -3,10 +3,18 @@ import React from 'react';
 const Landing = () => {
   return (
     <div class="wrapper">
-      <h1>Paws Your Day!</h1>
-        <div class="left-wrapper">
-            
+      <h1 class="title">Paws Your Day!</h1>
+      <div class="flex-container">
+        <div class="exercise-section">
+            <h1 class="title">Exercise for You</h1>
         </div>
+        <div class="general-section">
+            <h1 class="title">Rate Your Day</h1>
+        </div>
+        <div class="sleep-section">
+            <h1 class="title">Your Sleep Schedule</h1>
+        </div>
+      </div>
     </div>
   );
 };
