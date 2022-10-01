64c055cfe38f6befab31394a8c14deb313dcd7a2
bquach1 <quachbruce@gmail.com>
2022-10-01 14:04:52 -0500

    Add day rating functionality and renders for exercise/sleep

--- pawsyourday/src/screens/landing.js
+++ pawsyourday/src/screens/landing.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactSlider from 'react-slider';
 import Slider from '../components/sliders/slider';
+import { ChevronUp, ChevronDown } from 'react-feather';
 
 var exerciseVal = 5;
 var sleepVal = 10;
 
 const Landing = () => {
+  const [dayVal, setDayVal] = useState(0);
+
   return (
     <div class="wrapper">
       <h1 class="title">Paws Your Day</h1>
@@ -14,13 +17,20 @@ const Landing = () => {
         <div class="exercise-section">
             <h1 class="subtitle">Exercise for You</h1>
                 <div>           
-                    {(exerciseVal >= 5 && exerciseVal < 8) && <h2>You've been working out regularly.</h2>}  
-                    {exerciseVal < 5 && <h2>You've been working out less than average.</h2>}   
-                    {exerciseVal >= 8 && <h2>You've been super active!</h2>}
+                    {(dayVal >= 5 && dayVal < 8) && <h2>You've been working out regularly.</h2>}  
+                    {dayVal < 5 && <h2>You've been working out less than average.</h2>}   
+                    {dayVal >= 8 && <h2>You've been super active!</h2>}
                 </div>
                 {/* <div class="slider">
                     <Slider />
                 </div> */}
+        </div>
+
+        <div class="rate-bar">
+            <h1>Rate Your Day out of 10!</h1>
+            <ChevronUp onClick={() => (dayVal >= 10) ? setDayVal(10) : setDayVal(dayVal + 1)}/>
+            <ChevronDown onClick={() => (dayVal <= 0) ? setDayVal(0) : setDayVal(dayVal - 1)}/>
+            <h1>{dayVal}</h1>
         </div>
 
         <div class="general-section">
@@ -28,11 +38,11 @@ const Landing = () => {
         </div>
 
         <div class="sleep-section">
-            <h1 class="subtitle">Your Sleep Schedule</h1>
+            <h1 class="subtitle">Sleep Schedule</h1>
                 <div>
-                    {(sleepVal < 5) && <h2>You've been sleeping less than normal.</h2>}  
-                    {(sleepVal >= 5 && sleepVal < 7) && <h2>You've been sleeping okay.</h2>}   
-                    {sleepVal >= 7 && <h2>You've been sleeping a good amount!</h2>}
+                    {(dayVal < 5) && <h2>You've been sleeping less than normal.</h2>}  
+                    {(dayVal >= 5 && dayVal < 7) && <h2>You've been sleeping okay.</h2>}   
+                    {dayVal >= 7 && <h2>You've been sleeping a good amount!</h2>}
                 </div>
         </div>
 
