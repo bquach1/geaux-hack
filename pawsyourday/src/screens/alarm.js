3bf5150d2f0e2e412f881ad398c8b28c4a616b60
bquach1 <quachbruce@gmail.com>
2022-10-01 16:51:42 -0500

    Finish

--- pawsyourday/src/screens/alarm.js
+++ pawsyourday/src/screens/alarm.js
@@ -5,7 +5,7 @@ import { NavigationType, useNavigate } from "react-router-dom";
 const Alarm = () => {
   let navigate = useNavigate();
   return (
-    <div class="wrapper">
+    <div class="alarm">
         <ArrowLeft class="navigation-arrow" onClick={() => {{(navigate('/landing'))}}}/>
       <h1>Alarm clock page.</h1>
     </div>
